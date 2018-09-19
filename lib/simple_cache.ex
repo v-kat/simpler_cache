1af0bcff407f6ea516f2d137e7dbec55dd981084
Ivy Rogatko <ipr210lehigh@gmail.com>
2018-09-18 17:52:20 -0700

    specs and other minor cleanup

--- lib/simple_cache.ex
+++ lib/simple_cache.ex
@@ -5,29 +5,33 @@ defmodule SimpleCache do
   @table_name Application.get_env(:simple_cache, :cache_name, :simple_cache)
   @global_ttl_ms Application.get_env(:simple_cache, :global_ttl_ms, 10_000)
 
+  @spec get(any) :: nil | any
   def get(key) do
     :ets.lookup(@table_name, key) |> List.first()
   end
 
   @doc """
-  Inserts value or overwrites it
+  Inserts item or overwrites it
   """
+  @spec put(any, any) :: true
   def put(key, value) do
     {:ok, t_ref} = :timer.apply_after(@global_ttl_ms, :ets, :delete, [@table_name, key])
     :ets.insert(@table_name, {key, value, t_ref})
   end
 
   @doc """
-  Inserts new value into cache
+  Inserts new item into cache
   """
+  @spec insert_new(any, any) :: boolean
   def insert_new(key, value) do
     {:ok, t_ref} = :timer.apply_after(@global_ttl_ms, :ets, :delete, [@table_name, key])
     :ets.insert_new(@table_name, {key, value, t_ref})
   end
 
   @doc """
-  Deletes value from cache returns ok if doesn't exist also
+  Deletes item from cache returns ok if doesn't exist as well
   """
+  @spec delete(any) :: true
   def delete(key) do
     t_ref = :ets.lookup_element(@table_name, key, 3)
     :timer.cancel(t_ref)
@@ -38,7 +42,8 @@ defmodule SimpleCache do
   Updates existing value in cache based on old value.
   Warning the below may thrash a bit on heavy contention.
   """
-  def update_existing(key, passed_fn) do
+  @spec update_existing(any, function) :: boolean
+  def update_existing(key, passed_fn) when is_function(passed_fn) do
     with [{key, old_val, _timer}] <- :ets.take(@table_name, key),
          true <- SimpleCache.insert_new(key, passed_fn.(old_val)) do
       true
@@ -52,24 +57,21 @@ defmodule SimpleCache do
   end
 
   @doc """
-  Gets or stores a value based on a passed in function.
+  Gets or stores an item based on a passed in function.
   Warning the below may thrash a bit on heavy contention.
   """
-  def get_or_store(key, passed_fn) do
-    case :ets.lookup(@table_name, key) do
-      [] ->
-        new_val = passed_fn.()
-
-        case SimpleCache.insert_new(key, new_val) do
-          false ->
-            get_or_store(key, passed_fn)
-
-          true ->
-            new_val
-        end
-
-      [{key, val, _timer}] ->
+  @spec get_or_store(any, function) :: any
+  def get_or_store(key, passed_fn) when is_function(passed_fn) do
+    with [] <- :ets.lookup(@table_name, key),
+         new_val = passed_fn.(),
+         true <- SimpleCache.insert_new(key, new_val) do
+      new_val
+    else
+      [{_key, val, _timer}] ->
         val
+
+      false ->
+        get_or_store(key, passed_fn)
     end
   end
 
@@ -84,10 +86,19 @@ defmodule SimpleCache do
   @doc """
   Sets the ttl to a specific value in ms for a key
   """
-  def set_ttl_ms(key, time_ms) do
+  @spec set_ttl_ms(any, pos_integer) :: boolean
+  def set_ttl_ms(key, time_ms) when time_ms > 0 do
     t_ref = :ets.lookup_element(@table_name, key, 3)
-    :timer.cancel(t_ref)
-    {:ok, t_ref} = :timer.apply_after(time_ms, :ets, :delete, [@table_name, key])
-    :ets.update_element(@table_name, key, {3, t_ref})
+    _ = :timer.cancel(t_ref)
+    {:ok, new_t_ref} = :timer.apply_after(time_ms, :ets, :delete, [@table_name, key])
+
+    case :ets.update_element(@table_name, key, {3, new_t_ref}) do
+      true ->
+        true
+
+      false ->
+        _ = :timer.cancel(new_t_ref)
+        false
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule SimpleCache.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: [flags: [:unmatched_returns, :error_handling, :race_conditions, :underspecs]]
+      dialyzer: [flags: [:error_handling, :race_conditions, :underspecs]]
     ]
   end
 
